0edbb6c4ba824da0d25f3f6b2655c78c00bd0126
kirthykaa <ksk080104@gmail.com>
2024-06-23 23:58:49 +0530

    form update

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,11 +1,11 @@
-import './SearchForm.css'
+import './SearchForm.css';
 
-export default function SearchForm(){
-    return(
+export default function SearchForm() {
+  return (
     <form action="" id="search-form">
-        <input type="search" placeholder="search here..." name="search-box" id="search-box" />
-        <label for="search-box" className="fas fa-search"></label>
-        <i class="fas fa-times" id="close"></i>
-     </form>
-    )
+      <input type="search" placeholder="Search here..." name="search-box" id="search-box" />
+      <label htmlFor="search-box" className="fas fa-search"></label>
+      <i className="fas fa-times" id="close"></i>
+    </form>
+  );
 }
