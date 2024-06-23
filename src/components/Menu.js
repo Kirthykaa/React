47947212b5a37364d98339cbdaac2438d4d6d754
kirthykaa <ksk080104@gmail.com>
2024-06-23 23:55:56 +0530

    menu update

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import Header from "./Header";
 import SearchForm from "./SearchForm";
 import "./Menu.css";
@@ -6,52 +6,34 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { selectedPdts, setPdts } from "../redux/actions/menuActions";
 
-function Menu() {
+export default function Menu() {
   const navigate = useNavigate();
-  const menuData = useSelector((state) => state.allPdts.products);
-  const [menuItems, setMenuItem] = useState(menuData);
-  const [sltData, setSltData] = useState({});
   const dispatch = useDispatch();
+  
+  // Use useSelector to fetch the products from the Redux store
+  const menuData = useSelector((state) => state.allPdts.products);
+  
+  // Initialize state with menuData from Redux store
+  const [menuItems, setMenuItems] = useState(menuData);
 
-  console.log('menuData', menuData)
+  // Handle incrementing or decrementing item count
+  const updateMenuItemCount = useCallback((itemId, delta) => {
+    setMenuItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === itemId ? { ...item, count: Math.max(0, item.count + delta) } : item
+      )
+    );
+  }, []);
 
-  const onMenuAddHandler = (e, param) => {
-    if (param.count < 25) {
-      setMenuItem((prevState) => {
-        const newState = prevState.map((obj) => {
-          if (obj.id == param.id) {
-            return { ...obj, count: param.count + 1 };
-          }
-          return obj;
-        });
-        return newState;
-      });
-    }
-  };
-
-  const onMenuSubHandler = (e, param) => {
-    if (param.count > 0) {
-      setMenuItem((prevState) => {
-        const newState = prevState.map((obj) => {
-          if (obj.id == param.id) {
-            return { ...obj, count: param.count - 1 };
-          }
-          return obj;
-        });
-        return newState;
-      });
-    }
-  };
-
+  // Handle the logic whenever menuItems changes
   useEffect(() => {
     dispatch(setPdts(menuItems));
-    let totSlt = menuItems.filter((ele) => ele.count > 0);
-    console.log("selected Pdts :>> ", totSlt);
-    setSltData(totSlt);
-    dispatch(selectedPdts(totSlt));
-  }, [menuItems]);
+    const selectedItems = menuItems.filter((item) => item.count > 0);
+    dispatch(selectedPdts(selectedItems));
+  }, [menuItems, dispatch]);
 
-  const onChkoutHandler = () => {
+  // Handle checkout logic
+  const onCheckoutHandler = () => {
     setTimeout(() => navigate("/order"), 2000);
   };
 
@@ -60,50 +42,47 @@ function Menu() {
       <Header />
       <SearchForm />
       <section className="menu" id="menu">
-        <h3 className="sub-heading">our menu</h3>
-        <h1 className="heading">today's speciality</h1>
+        <h3 className="sub-heading">Our Menu</h3>
+        <h1 className="heading">Today's Speciality</h1>
         <div className="box-container">
-          {menuItems
-            ? menuItems.map((item, index) => {
-                return (
-                  <div key={index} className="menu-box">
-                    <div className="image">
-                      <img src={require(`../assets/images/${item.img.split('/').pop()}`)} alt="" />
-                    </div>
-                    <div className="content">
-                      <div>
-                        <h3>{item.name}</h3>
-                        <div className="stars">
-                          {Array.from(Array(item.star), (e, id) => {
-                            return <i key={id} className="fas fa-star"></i>;
-                          })}
-                        </div>
-                        <p>{item.describe}</p>
-                      </div>
-                      <div>
-                        <button
-                          className="topup-btn"
-                          onClick={(e) => onMenuSubHandler(e, item)}
-                        >
-                          -
-                        </button>
-                        <span className="item-count">{item.count}</span>
-                        <button
-                          className="topup-btn"
-                          onClick={(e) => onMenuAddHandler(e, item)}
-                        >
-                          +
-                        </button>
-                      </div>
-                    </div>
-                    <span className="price">Rs {item.price}</span>
+          {menuItems.map((item) => (
+            <div key={item.id} className="menu-box">
+              <div className="image">
+                {/* Using require to dynamically load images */}
+                <img src={require(`../assets/images/${item.img.split('/').pop()}`)} alt={item.name} />
+              </div>
+              <div className="content">
+                <div>
+                  <h3>{item.name}</h3>
+                  <div className="stars">
+                    {Array.from({ length: item.star }).map((_, index) => (
+                      <i key={index} className="fas fa-star"></i>
+                    ))}
                   </div>
-                );
-              })
-            : null}
+                  <p>{item.describe}</p>
+                </div>
+                <div className="counter">
+                  <button
+                    className="topup-btn"
+                    onClick={() => updateMenuItemCount(item.id, -1)}
+                  >
+                    -
+                  </button>
+                  <span className="item-count">{item.count}</span>
+                  <button
+                    className="topup-btn"
+                    onClick={() => updateMenuItemCount(item.id, 1)}
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+              <span className="price">Rs {item.price}</span>
+            </div>
+          ))}
         </div>
         <div className="chk-out-container">
-          <button className="topup-btn" onClick={() => onChkoutHandler()}>
+          <button className="topup-btn" onClick={onCheckoutHandler}>
             Checkout
           </button>
         </div>
@@ -111,5 +90,3 @@ function Menu() {
     </>
   );
 }
-
-export default Menu;
